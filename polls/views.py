9e8fa1343dd90cce0f70813ddc699ef21ac41894
shi-fujiwara <fuji.ktbl@gmail.com>
2025-05-22 10:07:14 +0000

    詳細ページとインデックスページのテンプレートを追加し、質問の詳細を表示する機能を実装

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.http import Http404
+from django.shortcuts import HttpResponse, get_object_or_404, render
 
-from .models import Question, Choice
+from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
