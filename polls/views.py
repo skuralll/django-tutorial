922dbfcbc0334a695890d53abf65fc99c36f57c8
shi-fujiwara <fuji.ktbl@gmail.com>
2025-05-22 09:46:30 +0000

    views.pyに最新の質問リストを表示する機能を追加し、各質問の詳細、結果、および投票機能を実装

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import HttpResponse
 
+from .models import Question, Choice
+
 
 def index(request):
-    return HttpResponse("Hello world you are at polls index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
